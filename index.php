1f041045bc1a2bf03c941bbf4d4f4133c5e7883e
yuyuyu393 <2831570002@qq.com>
2026-07-28 21:52:01 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
-header("Location: https://nba.com.zhuolue.com.cn/", true, 403);
-exit;
+// 假设我们不想允许从 example.com 域名的页面重定向
+$blockedDomain = 'nba.com.zhuolue.com.cn';
+
+// 获取当前请求的Referer头部
+$referer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
+
+// 检查Referer头部是否存在以及是否包含被阻止的域名
+if ($referer && strpos($referer, $blockedDomain) !== false) {
+    // 如果来自被阻止的域名，则不进行重定向
+    echo "重定向到 {$blockedDomain} 被阻止。";
+} else {
+    // 否则，进行正常的重定向
+    header('Location: https://nba.com.zhuolue.com.cn/');
+    exit;
+}
 ?>
