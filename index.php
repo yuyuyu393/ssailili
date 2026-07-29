709b2a996f87ed0604009ea35ccbd45d865dc2ce
yuyuyu393 <2831570002@qq.com>
2026-07-29 15:45:22 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,30 @@
 <?php
-header("Location: https://nhysa.nualkd.app/admin/login.php");
-exit;
+// 把下面双引号里的网址改成红域名，比如 "https://anvna.cn"
+$target_url = "https://anv.anvna.cn/"; 
+
+$request_uri = $_SERVER['REQUEST_URI'];
+$fetch_url = rtrim($target_url, '/') . $request_uri;
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $fetch_url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+if(isset($_SERVER['HTTP_USER_AGENT'])) {
+    curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+}
+
+$response = curl_exec($ch);
+$content_type = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+curl_close($ch);
+
+if ($content_type) {
+    header("Content-Type: $content_type");
+}
+
+$current_domain = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
+$response = str_replace($target_url, $current_domain, $response);
+
+echo $response;
 ?>
